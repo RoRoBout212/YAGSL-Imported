7c2268d57981823c354221ed398046168f022edc
Roshan Thadani <rorobout212@gmail.com>
2026-01-18 11:06:49 -0500

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,126 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import org.dyn4j.geometry.Rotation;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.OperatorConstants;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+  private final CANcoder backRightCC = new CANcoder(13);
+  private final CANcoder backLeftCC = new CANcoder(14);
+  private final CANcoder frontRightCC = new CANcoder(12);
+  private final CANcoder frontLeftCC = new CANcoder(11);
+  private final Joystick driveController =
+      new Joystick(OperatorConstants.DriveControllerPort);
+  private final Joystick steerController =
+      new Joystick(OperatorConstants.SteerControllerPort);
+  private final RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+    SmartDashboard.putString("AbsEncoderUnit", backLeftCC.getAbsolutePosition().getUnits());
+    SmartDashboard.putNumber("BackRightAbsEncoder",  Rotation2d.fromRotations(backRightCC.getPosition().getValueAsDouble()).getDegrees()%360.0);
+    SmartDashboard.putNumber("BackLeftAbsEncoder",  Rotation2d.fromRotations(backLeftCC.getPosition().getValueAsDouble()).getDegrees()%360.0);
+    SmartDashboard.putNumber("FrontRightAbsEncoder",  Rotation2d.fromRotations(frontRightCC.getPosition().getValueAsDouble()).getDegrees()%360.0);
+    SmartDashboard.putNumber("FrontLeftAbsEncoder",  Rotation2d.fromRotations(frontLeftCC.getPosition().getValueAsDouble()).getDegrees()%360.0);
+    SmartDashboard.putNumber("SteerControllerX",steerController.getX());
+    SmartDashboard.putNumber("SteerControllerY",steerController.getY());
+    SmartDashboard.putNumber("DriveControllerX",driveController.getX()); 
+    SmartDashboard.putNumber("DriveControllerY",driveController.getY());
+    
+  }
+  
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final SwerveSubsystem driveBase = new SwerveSubsystem();
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final Joystick driveController =
+      new Joystick(OperatorConstants.DriveControllerPort);
+    private final Joystick steerController =
+      new Joystick(OperatorConstants.SteerControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    log();
+  }
+
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
+  driveBase.getSwerveDrive(),
+  () -> driveController.getY(), 
+  () -> driveController.getX())
+  .withControllerRotationAxis(driveController::getX)
+  .deadband(OperatorConstants.DEADBAND)
+  .scaleTranslation(0.8)
+  .allianceRelativeControl(true);
+
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy()
+  .withControllerHeadingAxis(
+    steerController::getX,
+    steerController::getY)
+  .headingWhile(true);
+  
+  Command driveFieldOrientatedDirectAngle = driveBase.driveFieldOriented(driveDirectAngle);
+  
+  Command driveFieldOrientatedAngularVelocity = driveBase.driveFieldOriented(driveAngularVelocity);
+  private void configureBindings() {
+    driveBase.setDefaultCommand(driveFieldOrientatedDirectAngle);
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+  public void log(){
+  }
+}
